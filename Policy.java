81e0fe79abb64302cb7a35cb36f8e1f15a6b81a7
Nina Matsukura Bernardino <nmatsuk0@email.cpcc.edu>
2024-02-19 12:43:16 -0500

    Update Policy.java
    Added PolicyHolder

--- Policy.java
+++ Policy.java
@@ -1,3 +1,4 @@
+//Programmed by Nina Mastukura Bernardino
 //Program purpose: models an insurance policy for one person
 public class Policy{
    //Attributes specific to policy info only (no more holder info)
@@ -5,19 +6,23 @@ public class Policy{
    private String providerName;
    //Step 3 of Project 3: Add static field to keep track of # of Policy objects created
    private static int numOfPolicies = 0; 
+   //Step 4 of Project 3: add policyHolder
+   private PolicyHolder policyHolder;
       
    //Sets default values for attributes (no-arg constructor)
    public Policy(){
       policyNumber = 0;
       providerName = "";
       numOfPolicies ++;
+      policyHolder = null;
    }
    
    //Contructor to initialize all fields with defaults
-   public Policy(int policyNum, String provName){
+   public Policy(int policyNum, String provName, PolicyHolder holder){
       policyNumber = policyNum;
       providerName = provName;   
       numOfPolicies++;  
+      policyHolder = holder;
    }
    
    //Setters and Getters
